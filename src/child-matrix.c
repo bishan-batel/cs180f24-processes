3cfea277606783450388a7a93eb99275a154f05d
Kishan S Patel <bishan.batel@protonmail.com>
2024-10-09 19:57:03 -0700

    bruh

--- src/child-matrix.c
+++ src/child-matrix.c
@@ -1,10 +1,74 @@
-#include <errno.h>
-#include <stdio.h>   /* fopen, fscanf, fclose */
-#include <stdlib.h>  /* exit, malloc          */
+#include <stdint.h>
+#include <stdio.h>  /* fopen, fscanf, fclose */
+#include <stdlib.h> /* exit, malloc          */
+#include <string.h>
 #include <sys/shm.h>
 
+int32_t matrix_get(
+  const int32_t* values,
+  const size_t width,
+  const size_t column,
+  const size_t row
+) {
+  return values[row * width + column];
+}
 
-int main(int argc, char **argv)
-{
-   return child_matrix(argc, argv);
+void matrix_set(
+  int32_t* values,
+  const size_t width,
+  const size_t column,
+  const size_t row,
+  const int32_t v
+) {
+  values[row * width + column] = v;
+}
+
+int32_t child_matrix(const size_t argc, const char* argv[]) {
+  if (argc < 5) {
+    perror("Incorrect argument count");
+    return 1; // failure
+  }
+
+  int32_t shared_mem_id = 0;
+  uint32_t child_index = 0;
+  uint32_t row = 0, column = 0;
+
+  int32_t err = 0;
+  err += sscanf(argv[1], "%d", &shared_mem_id);
+  err += sscanf(argv[2], "%u", &child_index);
+  err += sscanf(argv[3], "%u", &row);
+  err += sscanf(argv[4], "%u", &column);
+
+  if (err < 4) {
+    perror("Failed to parse arugments");
+    return 1;
+  }
+
+  int32_t* const shared_memory = shmat(shared_mem_id, NULL, 0);
+
+  if (shared_memory == NULL) {
+    perror("Failed to get shared memory");
+    return 1;
+  }
+
+  const size_t width = (size_t)shared_memory[0];
+
+  const int32_t* const input_matrix = &shared_memory[1];
+
+  int32_t* const output_matrix = &shared_memory[1 + width * width];
+
+  int32_t sum = 0;
+
+  for (size_t i = 0; i < width; i++) {
+    sum += matrix_get(input_matrix, width, column, i) *
+           matrix_get(input_matrix, width, i, row);
+  }
+
+  matrix_set(output_matrix, width, column, row, sum);
+
+  return 0;
+}
+
+int main(int argc, const char* argv[]) {
+  return child_matrix(argc, argv);
 }
--- src/parent-matrix.c
+++ src/parent-matrix.c
@@ -1,12 +1,16 @@
-#include <stdio.h> /* fopen, fscanf, fclose */
+#include <stdint.h>
+#include <stdio.h>  /* fopen, fscanf, fclose */
 #include <stdlib.h> /* exit, malloc          */
+#include <errno.h>
+#include <string.h>
+#include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int *get_matrix(const char *filename, int *width) {
-  int value, *matrix;
-  FILE *fp;
+int32_t* get_matrix(const char* filename, size_t* width) {
+  int32_t value, *matrix;
+  FILE* fp;
 
   /* Open the file in text/translated mode */
   fp = fopen(filename, "rt");
@@ -16,8 +20,14 @@ int *get_matrix(const char *filename, int *width) {
   }
 
   /* Read the width and allocate the matrix */
-  fscanf(fp, "%d", width);
-  matrix = malloc(*width * *width * sizeof(int));
+  {
+    int32_t iwidth = 0;
+    fscanf(fp, "%d", &iwidth);
+    *width = (size_t)iwidth;
+  }
+
+  matrix = malloc(*width * *width * sizeof(int32_t));
+
   if (!matrix) {
     printf("Can't malloc matrix\n");
     fclose(fp);
@@ -26,8 +36,10 @@ int *get_matrix(const char *filename, int *width) {
 
   /* Read the vaules and put in the matrix */
   while (!feof(fp)) {
-    int result = fscanf(fp, "%d", &value);
-    if (result == -1) break;
+    int32_t result = fscanf(fp, "%d", &value);
+    if (result == -1) {
+      break;
+    }
     *matrix++ = value;
   }
   fclose(fp);
@@ -36,54 +48,148 @@ int *get_matrix(const char *filename, int *width) {
   return matrix - (*width * *width);
 }
 
-void print_matrix(int *matrix, int width) {
-  int i, size = width * width;
-  for (i = 0; i < size; i++) {
+void print_matrix(const int32_t* const matrix, const size_t width) {
+  const size_t size = width * width;
+
+  for (size_t i = 0; i < size; i++) {
     printf("%8i", matrix[i]);
-    if ((i + 1) % width == 0) printf("\n");
+    if ((i + 1) % width == 0) {
+      printf("\n");
+    }
   }
+
   printf("\n");
 }
 
-int main(int argc, const char **argv) {
-  int width; /* width of the matrix   */
-  int *matrix; /* the matrix read in    */
-
+int32_t main(const int32_t argc, const char* const argv[]) {
   if (argc < 3) {
     printf("Insufficient parameters supplied\n");
     return -1;
   }
 
+  const char* const child_program = argv[2];
+
   /* read in matrix values from file */
   /* don't forget to free the memory */
-  matrix = get_matrix(argv[1], &width);
+  size_t width; /* width of the matrix   */
+  int32_t* matrix = get_matrix(argv[1], &width);
 
   /* print the matrix */
   print_matrix(matrix, width);
 
-  const char *program = argv[2];
+  // NTQ=
+  // 52
 
-  int id = shmget(123, sizeof(int) * (width * width + 1), SHM_W);
+  const key_t shared_memory_id =
+    shmget(53, sizeof(int32_t) * (2 * width * width + 1), IPC_CREAT | 0644);
+
+  if (shared_memory_id == -1) {
+    perror("Failed to create shared memory");
+    free(matrix);
+    return 1;
+  }
+
+  int32_t* const shared_buffer = shmat(shared_memory_id, NULL, 0);
+
+  if (shared_buffer == (void*)-1) {
+    perror("Failed to get shared memory");
+    free(matrix);
+    return 1;
+  }
+
+  shared_buffer[0] = (int32_t)width;
+  // copy over the read matrix into shared memory
+  memcpy(&shared_buffer[1], matrix, sizeof(int32_t) * width * width);
+  // set the output matrix all to 0
+  memset(&shared_buffer[1 + width * width], 0, sizeof(int32_t) * width * width);
+
+  // dont need the original matrix
+  free(matrix);
 
   /* Fork a child for each matrix entry       */
   /* May need an array to hold the child pids */
-  for (int r = 0; r < width; r++) {
-    for (int c = 0; c < width; c++) {
+  pid_t* const child_pids = calloc(width * width, sizeof(pid_t));
 
-      execl(program, "", "", NULL);
+  for (size_t row = 0; row < width; row++) {
+    for (size_t column = 0; column < width; column++) {
 
-      /* Do forking and stuff */
+      const pid_t process_id = fork();
+
+      if (process_id < 0) {
+        perror("Failed to fork process");
+
+        if (shmdt(shared_buffer) == -1) {
+          perror("Failed to detach shared memory");
+        }
+        if (shmctl(shared_memory_id, IPC_RMID, NULL) == -1) {
+          perror("Failed to destory shared memory");
+        }
+        free(child_pids);
+        return 1;
+      }
+
+      if (process_id != 0) {
+        continue;
+      }
+
+      char shared_id_str[16] = {'\0'};
+      sprintf(shared_id_str, "%d", shared_memory_id);
+
+      char child_number_str[16] = {'\0'};
+      sprintf(child_number_str, "%d", (uint32_t)(width * row + column));
+
+      char row_str[16] = {'\0'};
+      sprintf(row_str, "%d", (uint32_t)row);
+
+      char column_str[16] = {'\0'};
+      sprintf(column_str, "%d", (uint32_t)column);
+
+      execl(
+        child_program,
+        child_program,
+        shared_id_str,
+        child_number_str,
+        row_str,
+        column_str,
+        NULL
+      );
     }
   }
 
-  /* wait for children */
+  // wait for children
+  for (size_t i = 0; i < width * width; i++) {
+    int32_t status = 0;
 
+    if (waitpid(child_pids[i], &status, 0) == -1) {
+      perror("Failed to wait for process");
+
+      if (shmdt(shared_buffer) == -1) {
+        perror("Failed to detach shared memory");
+      }
+      if (shmctl(shared_memory_id, IPC_RMID, NULL) == -1) {
+        perror("Failed to destory shared memory");
+      }
+      free(child_pids);
+      return 1;
+    }
+  }
+
+  // free child list
+  free(child_pids);
 
   /* print matrix from shared buffer */
-  print_matrix(buffer + 1 + width * width, width);
+  print_matrix(shared_buffer + 1 + width * width, width);
 
   /* cleanup */
-  free(matrix);
+  if (shmdt(shared_buffer) == -1) {
+    perror("Failed to detach shared memory");
+    return 1;
+  }
+
+  if (shmctl(shared_memory_id, IPC_RMID, NULL) == -1) {
+    perror("Failed to destory shared memory");
+    return 1;
+  }
 
   return 0;
 }
